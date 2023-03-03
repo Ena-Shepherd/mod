9142326511862c12f47620e70d51eb8bb50f3200
ENA <62568994+yannis-stefanelli@users.noreply.github.com>
2023-03-03 22:01:43 +0100

    project now runs on server + Nucleoid implementation + Polymer basic implementation

--- src/main/java/ena/testmod/items/ExampleItem.java
+++ src/main/java/ena/testmod/items/ExampleItem.java
@@ -10,15 +10,20 @@
 
 package ena.testmod.items;
 
+import org.jetbrains.annotations.Nullable;
+
+import eu.pb4.polymer.core.api.item.PolymerItem;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class ExampleItem extends Item {
+public class ExampleItem extends Item implements PolymerItem {
 
     public ExampleItem(Settings settings) {
         super(settings);
@@ -28,5 +33,15 @@ public class ExampleItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_BREAK, 1.0F, 1.0F);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    }
+    
+///////////////////////////////////////////////////////
+    
+    //client method
+    @Override
+    public Item getPolymerItem(ItemStack itemStack, @Nullable ServerPlayerEntity player) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'getPolymerItem'");
+        return Items.STICK;
     }
 }
--- src/main/java/ena/testmod/items/LightsaberItem.java
+++ src/main/java/ena/testmod/items/LightsaberItem.java
@@ -1,18 +1,23 @@
 
 package ena.testmod.items;
 
+import org.jetbrains.annotations.Nullable;
+
+import eu.pb4.polymer.core.api.item.PolymerItem;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-public class LightsaberItem extends Item {
+public class LightsaberItem extends Item implements PolymerItem {
     public LightsaberItem(Settings settings) {
         super(settings);
     }
@@ -35,6 +40,14 @@ public class LightsaberItem extends Item {
         stack.decrement(1);
     }
     return new TypedActionResult<>(ActionResult.SUCCESS, player.getStackInHand(hand));
+    }
+
+    @Override
+    public Item getPolymerItem(ItemStack itemStack, @Nullable ServerPlayerEntity player) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'getPolymerItem'");
+
+        return Items.TRIDENT;
     }
         
 }
